a4bd356141a1604bdf5fb5fcc386e0b1d766cf12
headway <headway@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-16 03:27:53 +0000

    git-svn-id: https://plugins.svn.wordpress.org/google-buzz-link/trunk@206311 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ google-buzz.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Plugin Name: Google Buzz
+ * Plugin URI: http://headwaymarketing.com/google-buzz-wordpress-plugin
+ * Description: A plugin that syndicates your blog feed to your Google Buzz profile.
+ * Version: 1.0
+ * Author: Headway Marketing
+ * Author URI: http://headwaymarketing.com
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+// add menu item
+add_action('admin_menu', 'gbuzz_menu_item');
+
+// configure menu item
+function gbuzz_menu_item() {
+	add_options_page('Google Buzz Settings', 'Google Buzz', 1, 'gbuzz', 'gbuzz_settings');
+	}
+
+// create page to manage settings
+function gbuzz_settings() { 
+
+    // variables for the field and option names 
+    $gbuzz_username_opt_name = 'gbuzz_username';
+    $hidden_field_name = 'submit_hidden_field';
+    $gbuzz_username_field = 'gbuzz_username';
+
+    // Read in existing option value from database
+    $gbuzz_username_val = get_option( $gbuzz_username_opt_name );
+
+    // See if the user has posted us some information
+    // If they did, this hidden field will be set to 'Y'
+    if( $_POST[ $hidden_field_name ] == 'Y' ) {
+        // Read their posted value
+        $gbuzz_username_val = $_POST[ $gbuzz_username_field ];
+
+        // Save the posted value in the database
+        update_option( $gbuzz_username_opt_name, $gbuzz_username_val );
+
+        // Put an options updated message on the screen
+		echo '<div class="updated"><p><strong>Settings Saved!</strong></p></div>';
+} ?>
+    <div class="wrap">
+        <h2>Google Buzz Settings</h2>
+        <p>Insert your Google Buzz username and your blog feed will be added to your Google Buzz stream.</p>
+        <form name="form1" method="post" action="">
+            <input type="hidden" name="<?php echo $hidden_field_name; ?>" value="Y">
+            <p>Google Buzz Username: <input type="text" name="<?php echo $gbuzz_username_field; ?>" value="<?php echo $gbuzz_username_val; ?>" /></p>
+            <p><input type="submit" name="Submit" value="Update Options" /></p>
+        </form>
+    </div>
+<?php } 
+
+function gbuzz_link() {
+    // variables for the field and option names 
+    $gbuzz_username_opt_name = 'gbuzz_username';
+    $hidden_field_name = 'submit_hidden_field';
+    $gbuzz_username_field = 'gbuzz_username';
+
+    // Read in existing option value from database
+    $gbuzz_username_val = get_option( $gbuzz_username_opt_name );
+	
+	// Print link to Google Buzz Profile
+	echo '<link rel="me" type="text/html" href="http://www.google.com/profiles/'.$gbuzz_username_val.'" />';
+	}
+add_action('wp_head', 'gbuzz_link'); 
+?>
---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== Plugin Name ===
+Contributors: headway
+Donate link: http://headwaymarketing.com/
+Tags: Google Buzz
+Requires at least: 2.0.0
+Tested up to: 2.9.1
+Stable tag: 1.0
+
+The Google Buzz plugin links your blog feed to your Google Buzz profile.
+
+== Description ==
+
+Link your WordPress blog feed to your Google Buzz profile with this little plugin. When your blog is re-crawled the claim will be verified and feeds associated with the site will be made available within Google Buzz for the verified user.     
+
+== Installation ==
+
+1. Upload 'google-buzz.php' to the '/wp-content/plugins/' directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Navigate to 'Settings > Google Buzz' and simply insert your Google Buzz username.
+
+== Frequently Asked Questions ==
+
+= Can't I just add the XFN link manually? =
+
+You certainly can, not everyone is so tech savvi.
+
+== Screenshots ==
+
+1. This screen shot shows the back-end where your Google Buzz username goes.
+
+== Changelog ==
+
+= 1.0 =
+* First Release
+
+== Upgrade Notice ==
+
+= 1.0 =
+No upgraded needed.
+
